278d91deee2df8b9b6105d98f4cfcec6a5102526
liangpengfei <liang_pengfei@sinosoft.com.cn>
2020-03-29 17:36:40 +0800

    CyclicBarrierDemo案例调整

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-##JUC-并发包
+## JUC-并发包
 > 学习并发包的相关内容，现对所学内容以及代码示例进行梳理和总结，巩固所学知识以便后续回顾。
-> 
 >
 1. [DCL单例模式-volatile](./src/main/java/pers/liangshan/demos/juc/SingletonDCLDemo.java) 
 2. [CountDownLatch：一个线程等多个线程](./src/main/java/pers/liangshan/demos/juc/CountDownLatchDemo.java)
--- src/main/java/pers/liangshan/demos/juc/CyclicBarrierDemo.java
+++ src/main/java/pers/liangshan/demos/juc/CyclicBarrierDemo.java
@@ -7,17 +7,20 @@ import java.util.concurrent.CyclicBarrier;
  * CyclicBarrier的字面意思是可循环(Cyclic) 使用的屏障(barrier).
  * 它要做的事情是,让一组线程到达一个屏障(也可以叫做同步点)时被阻塞,知道最后一个线程到达屏障时,
  * 屏障才会开门,所有被屏障拦截的线程才会继续干活,线程进入屏障通过CyclicBarrier的await()方法.
+ *
+ * 生活类比：长跑比赛，所有运动员准备就绪后，裁判开枪，开始比赛
+ *
  */
 public class CyclicBarrierDemo {
 
     public static void main(String[] args) {
 
-        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> System.out.println("**召唤神龙"));
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> System.out.println("裁判开枪，比赛开始。。"));
 
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= 8; i++) {
             int tempInt = i;
             new Thread(() -> {
-                System.out.println(Thread.currentThread().getName() + "\t 收集到了" + tempInt + "龙珠");
+                System.out.println(Thread.currentThread().getName() + "\t"+tempInt+"号选手准备就绪");
                 try {
                     //屏障拦截
                     cyclicBarrier.await();
